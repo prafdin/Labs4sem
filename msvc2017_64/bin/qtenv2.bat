8a89fffc1283df0378219455f1ca36cd8f5792b6
Ivan Pravdin <pravdin.biss@gmail.com>
2021-04-14 00:20:36 +0400

    add qt

---
+++ msvc2017_64/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=D:\Libraries\Qt\5.14.1\5.14.1\msvc2017_64\bin;%PATH%
+cd /D D:\Libraries\Qt\5.14.1\5.14.1\msvc2017_64
+echo Remember to call vcvarsall.bat to complete environment setup!
